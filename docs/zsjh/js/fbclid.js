46ba62b9683674a640eb6165ac3b63cc502816ae
jayhsu0114 <61579916+jayhsu0114@users.noreply.github.com>
2023-01-12 04:01:44 +0800

    js

--- docs/zsjh/js/fbclid.js
+++ docs/zsjh/js/fbclid.js
@@ -1,18 +1,19 @@
-var url = window.location.href;
+ // 取得網址
+ var url = window.location.href;
 
+ // 取得網址中的 fbclid
+ var fbclidValue = "";
+ if(url.indexOf("fbclid=") != -1) {
+     var start = url.indexOf("fbclid=") + 7;
+     var end = url.indexOf("&", start);
+     if(end == -1) {
+         end = url.length;
+     }
+     fbclidValue = url.substring(start, end);
+ }
 
-var fbclidValue = "";
-if(url.indexOf("fbclid=") != -1) {
-    var start = url.indexOf("fbclid=") + 7;
-    var end = url.indexOf("&", start);
-    if(end == -1) {
-        end = url.length;
-    }
-    fbclidValue = url.substring(start, end);
-}
+ // 輸出取得的 fbclid
+ console.log("fbclid: " + fbclidValue);
 
-
-console.log("fbclid: " + fbclidValue);
-
-
-document.getElementById("fbclid").value = fbclidValue;
+ // 將 fbclidValue 賦值給表單輸入欄位
+ document.getElementById("fbclid").value = fbclidValue;
