67716ee3a9082c194ace18e06488e3bf4a121b7d
jayhsu0114 <61579916+jayhsu0114@users.noreply.github.com>
2023-01-12 03:44:12 +0800

    fbclid

---
+++ docs/zsjh/js/fbclid.js
@@ -0,0 +1,18 @@
+var url = window.location.href;
+
+
+var fbclidValue = "";
+if(url.indexOf("fbclid=") != -1) {
+    var start = url.indexOf("fbclid=") + 7;
+    var end = url.indexOf("&", start);
+    if(end == -1) {
+        end = url.length;
+    }
+    fbclidValue = url.substring(start, end);
+}
+
+
+console.log("fbclid: " + fbclidValue);
+
+
+document.getElementById("fbclid").value = fbclidValue;
