1b156bcbc4021f7ce994911606a8ce8bc4c2d8c2
jayhsu0114 <61579916+jayhsu0114@users.noreply.github.com>
2023-09-22 10:12:47 +0800

    testjh

--- docs/testjh/js/fbclid.js
+++ docs/testjh/js/fbclid.js
@@ -1,11 +1,12 @@
-        // 获取当前页面的URL
-        var currentURL = window.location.href;
+// 获取当前页面的URL
+var currentURL = window.location.href;
 
-        // 解析URL，获取参数部分
-        var urlParams = new URL(currentURL);
+// 解析URL，获取参数部分
+var urlParams = new URL(currentURL);
 
-        // 获取特定参数的值
-        var paramValue = urlParams.searchParams.get('fbclid');
+// 获取特定参数的值
+var paramValue = urlParams.searchParams.get('fbclid');
 
-        // 将参数值输出到页面
-        document.write("fbclid 参数的值是：" + paramValue);
+// 将参数值设置到输入元素中
+var fbclidInput = document.getElementById('fbclid');
+fbclidInput.value = paramValue;
