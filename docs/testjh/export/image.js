9cabc71b54559b28592d714ac950d4d0d94dceba
jayhsu0114 <61579916+jayhsu0114@users.noreply.github.com>
2024-01-10 03:11:21 +0800

    換行

--- docs/testjh/export/image.js
+++ docs/testjh/export/image.js
@@ -23,17 +23,14 @@ function drawTextOnCanvas() {
         const maxWidth = canvas.width - 400;  // 400 是預留給左右的邊距
         const lineHeight = 25;  // 行高，可以根據需要調整
 
-// 消除所有換行符號
-const userInputWithoutNewlines = userInput.replace(/\n/g, '');
-
 // 計算文字總字數
-const totalChars = userInputWithoutNewlines.length;
+const totalChars = userInput.length;
 
-// 將文字分割成多行，每30個字元插入一個換行符號
+// 每30個字換一行
 const maxCharsPerLine = 30;
 let lines = [];
 for (let i = 0; i < totalChars; i += maxCharsPerLine) {
-    const line = userInputWithoutNewlines.substring(i, i + maxCharsPerLine);
+    const line = userInput.substring(i, i + maxCharsPerLine);
     lines.push(line);
 }
 
