da2e61ab354304ae9f6839cf98d100004fe9e7c0
jayhsu0114 <jayhsu0114@gmail.com>
2024-10-10 01:47:44 +0800

    signin human js

---
+++ docs/clientpages/membership/authentication/signin/js/post.js
@@ -0,0 +1,35 @@
+function handleSubmit(event) {
+    event.preventDefault(); // 防止表單提交預設行為
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('https://google-sheets-proxy-mk66ircp2a-uc.a.run.app//membership-humansignin', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json(); // 如果回應是成功的，返回 JSON 格式的數據
+        } else {
+            throw new Error('Network response was not ok.');
+        }
+    })
+    .then(data => {
+        // 獲取後端返回的 JWT token
+        const token = data.token;
+
+        // 將 token 存入 localStorage
+        localStorage.setItem('token', token);
+
+        // 根據後端返回的重定向路徑進行跳轉
+        window.location.href = data.redirectUrl;
+    })
+    .catch(error => {
+        console.error('Error during login:', error);
+        alert('登入失敗：無效的帳號或密碼。');
+    });
+}
