c686b32276705085fca0c43100d368de4e60e15f
jayhsu0114 <jayhsu0114@gmail.com>
2024-10-13 21:00:57 +0800

    fix id spell bug

--- docs/clientpages/membership/authentication/changepassword/js/post.js
+++ docs/clientpages/membership/authentication/changepassword/js/post.js
@@ -4,20 +4,20 @@ function handleSubmit(event) {
 
     // 獲取表單中的所有輸入值
     const password = document.getElementById('password').value;
-    const passwordcomfirm = document.getElementById('passwordcomfirm').value;
+    const passwordconfirm = document.getElementById('passwordconfirm').value; // 修正了拼写错误
     const userId = document.getElementById('userId').value;
 
     // 構建一個包含這些輸入值的物件
     const payload = {
         password: password,
         userId: userId,
-        passwordcomfirm: passwordcomfirm,
+        passwordconfirm: passwordconfirm, // 修正了拼写错误
     };
 
     console.log("Sending data:", payload); // 輸出要發送的資料
 
     // 使用 fetch 發送 POST 請求到後端
-    fetch('https://google-sheets-proxy-112185679321.us-central1.run.app/membership-forgetpassword/update-password', {  // 修改為你的後端驗證 API URL
+    fetch('https://google-sheets-proxy-mk66ircp2a-uc.a.run.app/membership-forgetpassword/update-password', {  // 修改為你的後端驗證 API URL
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -35,7 +35,7 @@ function handleSubmit(event) {
         // 根據回應中的 success 字段決定是否成功
         if (data.success) {
             console.log('Success:', data);
-            window.location.href = '/clientpages/membership/authentication/signin';
+            window.location.href = '/clientpages/membership/authentication/signin'; // 跳轉到登入頁面
         } else {
             alert(`驗證失敗：${data.message}`);
             console.log('Error:', data.message);
