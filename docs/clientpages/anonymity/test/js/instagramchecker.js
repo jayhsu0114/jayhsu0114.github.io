588f7b75698b17294b64fbaf9661a9b25d4c1212
jayhsu0114 <jayhsu0114@gmail.com>
2024-10-17 19:28:21 +0800

    刪除測試頁面

--- docs/clientpages/anonymity/test/js/instagramchecker.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var userAgent = navigator.userAgent.toLowerCase();
-    if (userAgent.indexOf('instagram') === -1) {
-        window.location.href = '/clientpages/wrongpage';
-    }
-});
