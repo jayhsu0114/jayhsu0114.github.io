0e000d599a0ad156429b29f07736f47ca7dd7617
jayhsu0114 <jayhsu0114@gmail.com>
2024-10-17 22:17:13 +0800

    soter 路徑錯誤

--- docs/clientpages/membership/research/choose/js/soter.js
+++ docs/clientpages/membership/research/choose/js/soter.js
@@ -9,7 +9,7 @@ const school = sessionStorage.getItem('school');
 // 如果沒有 token，重定向到登入頁面
 if (!token) {
     console.log('No token found. Redirecting to login page...');
-    window.location.href = '/managementpages/authentication/signin'; // 調整為登入頁面的實際路徑
+    window.location.href = '/clientpages/membership/authentication/signin'; // 調整為登入頁面的實際路徑
 } else {
     // 如果有 token，將 token 放入 Authorization header 中進行驗證
     console.log('Token found. Verifying token...');
@@ -75,6 +75,6 @@ if (!token) {
         console.error('Error validating token:', error);
         // 如果驗證過程中出現錯誤，重定向到登入頁面
         console.log('Error during token validation. Redirecting to login page...');
-        window.location.href = '/managementpages/authentication/signin'; // 根據實際情況調整登入頁面的路徑
+        window.location.href = '/clientpages/membership/authentication/signin'; // 根據實際情況調整登入頁面的路徑
     });
 }
