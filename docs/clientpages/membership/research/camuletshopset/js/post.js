9ef3a6e88dc2f2a25ec4c0481140cab365d05815
jayhsu0114 <jayhsu0114@gmail.com>
2024-11-06 19:48:04 +0800

    訊息驗證

--- docs/clientpages/membership/research/camuletshopset/js/post.js
+++ docs/clientpages/membership/research/camuletshopset/js/post.js
@@ -1,5 +1,7 @@
 // 載入頁面後在 localStorage 中新增 shopcode 值
 localStorage.setItem('shopcode', 'camulet20241106');
 
-// 在 footer 之前插入店家驗證設置成功的訊息
-document.querySelector('footer').insertAdjacentHTML('beforebegin', '<center><p>店家驗證設置成功</p></center>');
+// 檢查是否成功新增 shopcode，然後在 footer 之前插入店家驗證設置成功的訊息
+if (localStorage.getItem('shopcode') === 'camulet20241106') {
+    document.querySelector('footer').insertAdjacentHTML('beforebegin', '<center><p>店家驗證設置成功</p></center>');
+}
