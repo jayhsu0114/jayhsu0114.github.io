f813200fbd19b9840cfaaa397369ef99dd8b25af
jayhsu0114 <jayhsu0114@gmail.com>
2024-11-06 19:09:53 +0800

    shOPSET

--- docs/clientpages/membership/research/camuletshopset/js/post.js
+++ docs/clientpages/membership/research/camuletshopset/js/post.js
@@ -1,2 +1,5 @@
 // 載入頁面後在 localStorage 中新增 shopcode 值
 localStorage.setItem('shopcode', 'camulet20241106');
+
+// 在 footer 之前插入店家驗證設置成功的訊息
+document.querySelector('footer').insertAdjacentHTML('beforebegin', '<center><p>店家驗證設置成功</p></center>');
